92d6af5d1c828d252200e777db2423cfc1f378cb
Jatin grover <akazasama@Jatins-MacBook-Air.local>
2022-10-30 07:13:06 +0530

    added the search form

--- src/Search.js
+++ src/Search.js
@@ -1,5 +1,64 @@
 import React from "react";
+import "./Search.css";
 
 export function Search() {
-  return <div>Search Route</div>;
+  return (
+    <div className="border rounded mx-auto w-75 p-3">
+      <h3>Business search</h3>
+      <form className="row row-cols-2 g-5">
+        <div className="col-12 ">
+          <label for="keyword" className="text-start required">
+            Keyword
+          </label>
+          <input type="text" className="form-control" id="keyword" />
+        </div>
+        <div className="col-6">
+          <label for="distance" className="form-label">
+            Distance
+          </label>
+          <input type="text" className="form-control" id="distance" />
+        </div>
+        <div className="col-6">
+          <label for="category" className="required">
+            Category
+          </label>
+
+          <select id="location" className="form-select form-control">
+            <option selected>Default</option>
+            <option>Arts & Entertainment</option>
+            <option>Health & Medical</option>
+            <option>Food</option>
+            <option>Professional Services</option>
+          </select>
+        </div>
+        <div className="col-12">
+          <label for="location" className="required">
+            Location
+          </label>
+          <input type="text" id="location" className="form-control" />
+        </div>
+        <div className="col-12">
+          <div className="form-check">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="gridCheck"
+            />
+            <label className="form-check-label" for="gridCheck">
+              Auto-detect my location
+            </label>
+          </div>
+        </div>
+        <div className="col-12  justify-content-between text-center">
+          <button type="submit" className="btn btn-danger m-2">
+            Sign in
+          </button>
+
+          <button type="submit" className="btn btn-primary m-2">
+            Clear
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 }
